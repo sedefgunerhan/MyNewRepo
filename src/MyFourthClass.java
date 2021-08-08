e5bb3f6b1f746cea4d4f30bcbaaf3f8fa55df9f5
Sedef Gunerhan <sedefgunerhan@gmail.com>
2021-08-08 15:13:00 -0400

    codes

--- src/MyFourthClass.java
+++ src/MyFourthClass.java
@@ -1,8 +1,8 @@
 public class MyFourthClass {
     public static void main(String[] args) {
         int x = 5;
-        int sum = x + 2;
-        System.out.println(sum);
+        int sum = x + 5;
+        System.out.println(sum);}
 
 
 }
